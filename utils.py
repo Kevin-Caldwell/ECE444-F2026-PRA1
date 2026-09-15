504b1dcf7caefccd249912c9cb119e670dde2350
Kevin Caldwell <kevinc2004@gmail.com>
2026-09-15 11:43:00 -0400

    Fix utils.py using unit tests

--- utils.py
+++ utils.py
@@ -1,5 +1,9 @@
+import math
+
 class Utils:
-  def reversed(x: int) -> int:
+  def reversed(x) -> int:
+    if type(x) != int:
+      raise TypeError
     res = 0
     while x > 0:
       digit = x % 10
@@ -8,17 +12,20 @@ class Utils:
     
     return res
 
-  def formatter(x: int) -> (int, int):
+  def formatter(x) -> tuple[int, int]:
+    if type(x) != int:
+      raise TypeError
+    
     base_2 = 0
     pow_2_temp = x
-    while pow_2_temp > 0:
-      base_2 = base_2 * 10 + (pow_2_temp & 1)
+    for i in range(math.ceil(math.log2(x))):
+      base_2 += (pow_2_temp & 1) * 10**i
       pow_2_temp = pow_2_temp >> 1
 
     base_8 = 0
     pow_8_temp = x
-    while pow_8_temp > 0:
-      base_8 = base_8 * 10 + (pow_8_temp & 0h7)
+    for i in range(math.ceil(math.log2(x) / 3)):
+      base_8 = base_8 + (pow_8_temp & 7) * 10**i 
       pow_8_temp = pow_8_temp >> 3
     
     return (base_2, base_8)
